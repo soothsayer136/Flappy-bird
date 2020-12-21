3a3df6cc2e34892618b7a16ead3663845e630f27
soothsayer136 <bikrum.katwal@gmail.com>
2020-12-22 00:05:07 +0545

    canvas and drawing image

---
+++ script.js
@@ -0,0 +1,19 @@
+var canvas = $('#myCanvas');
+var ctx = canvas.getContext('2d');
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+
+bird.src = "Images/bird.png";
+bg.src = "Images/bg.png";
+fg.src = "Images/fg.png";
+pipeNorth.src = "Images/pipeNorth.png";
+pipeSouth.src = "Images/pipeSouth.png";
+
+
+
+  
